73d8a282d3d2da8a24f7824fc61c2d5841fd0808
LiuNine <68070286@kmitl.ac.th>
2026-09-23 23:44:50 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,5 @@
+age = int(input("Please tell me your age: "))
+print(f"You are currently {age} years old")
+for i in range(1,4):
+    year = 10 * i
+    print(f"In {year} years, you'll be {year + age} years old.")
